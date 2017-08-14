33cb372c24595d710c9b92fca3e6c618ba3788c8
Justin R. Wilson <jrw972@gmail.com>
2017-08-14 11:10:23 -0500

    Update Cloud Functions to use Cloud SQL

--- CloudFunctions/config/index.js
+++ CloudFunctions/config/index.js
@@ -1,18 +1,12 @@
 /*
-*
-*
-*Cloud function for subscribing to manifold-configuration topic
-*
-*
-*/
+ *
+ *
+ * Cloud function for subscribing to manifold-configuration topic
+ *
+ *
+ */
 
-
-// Specifying datastore requirement in GCP project
-const Datastore = require('@google-cloud/datastore');
-
-// Instantiates a client
-const datastore = Datastore();
-
+var mysql = require('mysql');
 
 // [START functions_pubsub_subscribe]
 /**
@@ -24,142 +18,65 @@ const datastore = Datastore();
  * @param {function} The callback function.
  */
 exports.subscribe = function subscribe (event, callback) {
-  const pubsubMessage = event.data;
+    const pubsubMessage = event.data;
 
-  // We're just going to log the message to prove that it worked!
-  const message = Buffer.from(pubsubMessage.data, 'base64').toString();
-  
-  console.log(message);
-  
-  var jsonData = JSON.parse(message);
-  
-  
-  if (!jsonData) {
-    throw new Error('message-payload is empty!');
-  }
-  //parsing the message for updating/creating the 'Valve' kind entity
-  createEntity(jsonData);
+    // We're just going to log the message to prove that it worked!
+    const message = Buffer.from(pubsubMessage.data, 'base64').toString();
 
-  // Don't forget to call the callback!
-  callback();
+    console.log(message);
+
+    var jsonData = JSON.parse(message);
+
+    if (!jsonData) {
+        throw new Error('message-payload is empty!');
+    }
+    //parsing the message for updating/creating the 'Valve' kind entity
+    createEntity(jsonData);
+
+    // Don't forget to call the callback!
+    callback();
 };
 // [END functions_pubsub_subscribe]
 
 // [START createEntity]
 function createEntity (jsonData) {
-  var manifold_key = jsonData.manifold_sn;
-  
-  for(var i = 0; i < jsonData.stations.length; i++){
+    var connection = mysql.createConnection({
+        socketPath: '/cloudsql/nexmatixmvp:us-central1:nexmatixmvd',
+        user: 'cfuser',
+        database: 'smartdevice_repo'
+    });
 
-      console.log("Config: iteration-"+i);
+    connection.connect();
 
-	  var station = jsonData.stations[i];
-	  
-	  var kind = "Valve";
-	  var entityKey = station.valve_sn;
-	  var request_for_key = JSON.parse("{\"kind\":\"".concat(kind).concat("\", \"key\":\"").concat(entityKey).concat("\"}"));
-	  const key = getKeyFromRequestData(request_for_key);
+    for(var i = 0; i < jsonData.stations.length; i++){
 
-	  var entity = {
-		key: key,
-		data: [
-			{
-				name: 'station_num',
-				value: station.station_num
-			},
-			{
-				name: 'update_time',
-				value: station.update_time
-			},
-			{
-				name: 'sku',
-				value: station.sku
-			},
-			{
-				name: 'fab_date',
-				value: station.fab_date
-			},
-			{
-				name: 'ship_date',
-				value: station.ship_date
-			}
-		]
-		};
+        console.log("Config: iteration-"+i);
 
-		//function to add entities
-		addEntity(entity);
-	}
-  }
-  
+	var station = jsonData.stations[i];
+
+	var entity = {
+            manifold_sn: jsonData.manifold_sn,
+            timestamp: new Date(jsonData.timestamp),
+
+            fab_date: station.fab_date,
+	    ship_date: station.ship_date,
+	    sku: station.sku,
+	    station_num: station.station_num,
+            valve_sn: station.valve_sn,
+            ccl: station.ccl
+	};
+
+        connection.query('INSERT INTO valve SET ? on duplicate key update ?', [entity, entity], function(error, results, fields) {
+            if (error) console.log(error);
+            else console.log("inserted successfully");
+        });
+    }
+
+    connection.end();
+}
 //[END createEntity]
 
-// [START addEntity]
-function addEntity (entity) {
-  datastore.save(entity)
-    .then(() => {
-      console.log(`an Entity ${entity.key.id} created successfully.`);
-    })
-    .catch((err) => {
-      console.error('ERROR:', err);
-    });
-}
-// [END add_entity]
-
-/**
- * Gets a Datastore key from the kind/key pair in the request.
- *
- * @param {object} requestData Cloud Function request data.
- * @param {string} requestData.key Datastore key string.
- * @param {string} requestData.kind Datastore kind.
- * @returns {object} Datastore key object.
- */
-function getKeyFromRequestData (requestData) {
-  if (!requestData.key) {
-    throw new Error('Key not provided. Make sure you have a "key" property in your request');
-  }
-
-  if (!requestData.kind) {
-    throw new Error('Kind not provided. Make sure you have a "kind" property in your request');
-  }
-
-  return datastore.key([requestData.kind, requestData.key]);
-}
-
-/**
- * Creates and/or updates a record.
- *
- * @example
- * gcloud alpha functions call set --data '{"kind":"Task","key":"sampletask1","value":{"description": "Buy milk"}}'
- *
- * @param {object} req Cloud Function request context.
- * @param {object} req.body The request body.
- * @param {string} req.body.kind The Datastore kind of the data to save, e.g. "Task".
- * @param {string} req.body.key Key at which to save the data, e.g. "sampletask1".
- * @param {object} req.body.value Value to save to Cloud Datastore, e.g. {"description":"Buy milk"}
- * @param {object} res Cloud Function response context.
- */
-exports.set = function set (req, res) {
-  // The value contains a JSON document representing the entity we want to save
-  if (!req.body.value) {
-    throw new Error('Value not provided. Make sure you have a "value" property in your request');
-  }
-
-  const key = getKeyFromRequestData(req.body);
-  const entity = {
-    key: key,
-    data: req.body.value
-  };
-
-  return datastore.save(entity)
-    .then(() => res.status(200).send(`Entity ${key.path.join('/')} saved.`))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send(err);
-      return Promise.reject(err);
-    });
-};
-
 /*
-gcloud beta functions deploy manifold-configuration-subscriber --entry-point subscribe --stage-bucket nexmatix-staging-bucket --trigger-topic manifold-configuration
+  gcloud beta functions deploy manifold-configuration-subscriber --entry-point subscribe --stage-bucket nexmatix-staging-bucket --trigger-topic manifold-configuration
 
 */
--- CloudFunctions/state/index.js
+++ CloudFunctions/state/index.js
@@ -1,19 +1,12 @@
 /*
-*
-*
-*Cloud function for subscribing to manifold-state topic for status
-*
-*
-*/
+ *
+ *
+ * Cloud function for subscribing to manifold-state topic for status
+ *
+ *
+ */
 
-// Specifying datastore requirement in GCP project
-const Datastore = require('@google-cloud/datastore');
-
-// Instantiates a client
-const datastore = Datastore();
-
-//enums
-const KIND_VALVE_STATUS = "ValveStatus"
+var mysql = require('mysql');
 
 // [START functions_pubsub_subscribe]
 /**
@@ -45,103 +38,59 @@ exports.subscribe = function subscribe (event, callback) {
 }
 // [END functions_pubsub_subscribe]
 
+// [START createEntity]
 function createEntity(jsonData){
     console.log("State: Entered createEntity...");
     var manifold_sn = jsonData.manifold_sn;
+    var timestamp = new Date(jsonData.timestamp)
 
     console.log("station_count:"+jsonData.stations.length);
 
+    var connection = mysql.createConnection({
+        socketPath: '/cloudsql/nexmatixmvp:us-central1:nexmatixmvd',
+        user: 'cfuser',
+        database: 'smartdevice_repo'
+    });
+
+    connection.connect();
+
     var station_index;
     for(station_index in jsonData.stations){
-        //new transaction started for i'th station
-        const transaction = datastore.transaction();
-
         station = jsonData.stations[station_index];
         const station_num = station.station_num;
         const valve_sn = station.valve_sn;
-        var valveStatusKey = manifold_sn + "." + station_num + "." + valve_sn;
-        var retrieved_key_status = datastore.key([KIND_VALVE_STATUS, valveStatusKey]);
 
         console.log("state: station_index:"+station_index);
         console.log("State: station-"+JSON.stringify(station));
         console.log("State: station_num-"+station_num);
         console.log("State: valve_sn-"+valve_sn);
-        console.log("State: valveStatusKey-"+valveStatusKey);
-        console.log("State: retrieved_key_status-"+retrieved_key_status);
 
-        return transaction.run()
-            // fetch valve status entity
-            .then (()=>transaction.get(retrieved_key_status))
-            .then((results)=> {
-                const retrieved_entity = results[0];
-                // If valve status entity exists
-                if(retrieved_entity){
-                    //update
-                    console.log("State: Updated a valve Status Entity with key:"+retrieved_key_status.name);
-                    var entity = createValveStatusEntity(station, manifold_sn, retrieved_key_status);
-                    return transaction.save(entity);
-                }else{
-                    // Insert valve status
-                    console.log("State: VStatusKey_retrieved:"+retrieved_key_status.name);
-                    //console.log("Created a valve Status Entity with key:"+retrieved_key.name);
-                    var entity = createValveStatusEntity(station, manifold_sn, retrieved_key_status);
-                    return transaction.save(entity);
-                }
-            })
-            .then(()=> {
-               transaction.commit();
-               console.log("State: Transaction Committed");
-            })
-            .catch((exception)=> {
-                transaction.rollback();
-                console.log("State: Transaction Rolledback:"+exception);
-            });
-        }
+        var entity = {
+            valve_sn: station.valve_sn,
+            manifold_sn: manifold_sn,
+            station_num: station.station_num,
+            cc: station.cc,
+            ccl: station.ccl,
+            timestamp: timestamp,
+            input: station.input,
+            pp: station.pp,
+            p_fault: station.p_fault,
+            leak: station.leak
+        };
+        console.log("State: Creating ValveStatusEntity..." + JSON.stringify(entity));
+
+        connection.query('INSERT INTO valve_status SET ? on duplicate key update ?', [entity, entity], function(error, results, fields) {
+            if (error) console.log(error);
+            else console.log("inserted successfully");
+        });
     }
 
-function createValveStatusEntity(jsonData, manifold_sn, key){
-    message_time = new Date().getTime();
-    entity = {
-        key: key,
-        data: [
-           {
-                name:"valve_sn", value:jsonData.valve_sn
-           },
-           {
-                name:"manifold_sn", value:manifold_sn
-           },
-           {
-                name:"station_num", value:jsonData.station_num
-           },
-           {
-                name:"cc", value:jsonData.cc
-           },
-           {
-                name:"ccl", value:jsonData.ccl
-           },
-           {
-                name:"update_time", value:message_time
-           },
-           {
-                name:"input", value:jsonData.input
-           },
-           {
-                name:"pp", value:jsonData.pp
-           },
-           {
-                name:"p_fault", value:jsonData.p_fault
-           },
-           {
-                name:"leak", value:jsonData.leak
-           }
-        ]
-    };
-    console.log("State: Creating ValveStatusEntity..." + JSON.stringify(entity));
-    return entity;
+    connection.end();
 }
+//[END createEntity]
 
 /*
 
-gcloud beta functions deploy manifold-state-subscriber --entry-point subscribe --stage-bucket nexmatix-staging-bucket --trigger-topic manifold-state
+  gcloud beta functions deploy manifold-state-subscriber --entry-point subscribe --stage-bucket nexmatix-staging-bucket --trigger-topic manifold-state
 
- */
+*/
